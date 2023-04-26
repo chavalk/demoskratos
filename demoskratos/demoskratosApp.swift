c08e5895fe91cb847d7d1ef0fe24b966276678ad
chavalk <chavalk@hotmail.com>
2023-04-26 16:01:38 -0500

    First commit.

---
+++ demoskratos/demoskratosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoskratosApp.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct demoskratosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
