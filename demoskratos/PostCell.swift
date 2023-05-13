0d68232e5dd0b9166a5e58ab76c90f2236e7bd82
chavalk <chavalk@hotmail.com>
2023-05-12 20:07:55 -0500

    Add post cell file.

---
+++ demoskratos/PostCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostCell.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 5/12/23.
+//
+
+import SwiftUI
+
+struct PostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCell()
+    }
+}
