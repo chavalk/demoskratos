144caf97c55e6c7b0777145a236209f543958c9e
chavalk <chavalk@hotmail.com>
2023-05-27 10:21:52 -0500

    Add circle image file.

---
+++ demoskratos/Views/Helpers/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 5/27/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
