f8513ad7593aae210e7dbaaf42aa19e5c7be0cf1
chavalk <chavalk@hotmail.com>
2023-05-27 11:30:16 -0500

    Comnplete circle image.

--- demoskratos/Views/Helpers/CircleImage.swift
+++ demoskratos/Views/Helpers/CircleImage.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 275, height: 275)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("texas-house-123"))
     }
 }
