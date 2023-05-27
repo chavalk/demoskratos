404be5a4ec3e5e4d1fc28d7ed03c815034c5f1cc
chavalk <chavalk@hotmail.com>
2023-05-26 20:36:29 -0500

    Add representative profile view file.

---
+++ demoskratos/Views/Profile/RepresentativeProfile.swift
@@ -0,0 +1,20 @@
+//
+//  RepresentativeProfile.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 5/26/23.
+//
+
+import SwiftUI
+
+struct RepresentativeProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RepresentativeProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        RepresentativeProfile()
+    }
+}
