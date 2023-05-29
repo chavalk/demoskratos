a8900b49eda6aaf608d195e4381183820982c667
chavalk <chavalk@hotmail.com>
2023-05-29 17:50:16 -0500

    Add image, name and text to recent votes row.

--- demoskratos/Views/Profile/RecentVotesRow.swift
+++ demoskratos/Views/Profile/RecentVotesRow.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct RecentVotesRow: View {
     var body: some View {
-        VStack {
-            Text("Voted For Senate Concurrent Resolution No. 61 instructing the enrolling clerk of the senate to make a correction in Senate Bill No. 1725.")
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Image("texas-house-123")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 56, height: 56)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Diego M. Bernal")
+                        .font(.headline)
+                    
+                    Text("Voted For Senate Concurrent Resolution No. 61 instructing the enrolling clerk of the senate to make a correction in Senate Bill No. 1725.")
+                }
+                
+            }
+            
             
             Divider()
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
