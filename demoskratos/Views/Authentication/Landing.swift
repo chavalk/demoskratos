3371ca9e2cc7807cca36b395265938668300466f
chavalk <chavalk@hotmail.com>
2023-06-03 10:58:38 -0500

    Add landing view file.

---
+++ demoskratos/Views/Authentication/Landing.swift
@@ -0,0 +1,20 @@
+//
+//  Landing.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 6/3/23.
+//
+
+import SwiftUI
+
+struct Landing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Landing_Previews: PreviewProvider {
+    static var previews: some View {
+        Landing()
+    }
+}
