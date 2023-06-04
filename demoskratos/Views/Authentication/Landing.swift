82b13cc4a2b9a84a824eb8cfcc4c77e7d6392436
chavalk <chavalk@hotmail.com>
2023-06-04 16:08:54 -0500

    Add navigation stack to landing view.

--- demoskratos/Views/Authentication/Landing.swift
+++ demoskratos/Views/Authentication/Landing.swift
@@ -9,47 +9,49 @@ import SwiftUI
 
 struct Landing: View {
     var body: some View {
-        VStack(spacing: 60) {
-            
-            Spacer()
-            
-            VStack(alignment: .center, spacing: 25) {
-                Text("Demoskratos")
-                    .font(.system(size: 60))
-                    .fontWeight(.bold)
+        NavigationStack {
+            VStack(spacing: 60) {
                 
-                Text("Stay up to date with your elected representatives.")
-                    .font(.system(size: 30))
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-            }
-            
-            Spacer()
-            
-            VStack(spacing: 15) {
-                Button {
-                    
-                } label: {
-                    Text("Create Account")
-                        .foregroundColor(.white)
-                        .fontWeight(.semibold)
-                        .frame(width: UIScreen.main.bounds.width - 48, height: 50)
-                }
-                .background(Color.black)
-                .cornerRadius(25)
+                Spacer()
                 
-                Button {
+                VStack(alignment: .center, spacing: 25) {
+                    Text("Demoskratos")
+                        .font(.system(size: 60))
+                        .fontWeight(.bold)
                     
-                } label: {
-                    Text("Log in")
+                    Text("Stay up to date with your elected representatives.")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
                         .foregroundColor(.black)
-                        .fontWeight(.semibold)
-                        .frame(width: UIScreen.main.bounds.width - 48, height: 50)
                 }
-                .overlay(
-                    RoundedRectangle(cornerRadius: 25)
-                        .stroke(Color.black, lineWidth: 1)
-                )
+                
+                Spacer()
+                
+                VStack(spacing: 15) {
+                    Button {
+                        
+                    } label: {
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                    }
+                    .background(Color.black)
+                    .cornerRadius(25)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Log in")
+                            .foregroundColor(.black)
+                            .fontWeight(.semibold)
+                            .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+                }
             }
         }
     }
