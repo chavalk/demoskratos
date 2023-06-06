e0d30a5d3c433e0feaf38a9945a328580ab879b2
chavalk <chavalk@hotmail.com>
2023-06-05 22:02:39 -0500

    Add email address input field, password input field, and state variables for each input field.

--- demoskratos/Views/Authentication/Registration.swift
+++ demoskratos/Views/Authentication/Registration.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct Registration: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             Text("Create new account")
                 .font(.system(size: 30))
                 .fontWeight(.semibold)
+            
+            VStack(spacing: 32) {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Email Address")
+                        .fontWeight(.semibold)
+                        .font(.footnote)
+                    
+                    TextField("Enter your email address", text: $email)
+                        .foregroundColor(.white)
+                    
+                    Rectangle()
+                        .foregroundColor(Color(.init(white: 1, alpha: 0.3)))
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 0.7)
+                }
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Password")
+                        .fontWeight(.semibold)
+                        .font(.footnote)
+                    
+                    TextField("Enter your password", text: $password)
+                        .foregroundColor(.white)
+                    
+                    Rectangle()
+                        .foregroundColor(Color(.init(white: 1, alpha: 0.3)))
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 0.7)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
         }
     }
 }
