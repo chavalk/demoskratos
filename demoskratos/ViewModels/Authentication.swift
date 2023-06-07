8413fe704851da40b18febd3fa501b920e42220d
chavalk <chavalk@hotmail.com>
2023-06-06 21:07:36 -0500

    Rename auth view model to authentication.

--- demoskratos/ViewModels/Authentication.swift
+++ demoskratos/ViewModels/Authentication.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Firebase
 
-class AuthViewModel: ObservableObject {
+class Authentication: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     
     init() {
