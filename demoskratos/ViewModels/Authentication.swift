fecdb2ab0a87291dfe9aea6013e454bf53436764
chavalk <chavalk@hotmail.com>
2023-06-06 21:06:19 -0500

    Add user session published var and initializer to auth view model.

--- demoskratos/ViewModels/Authentication.swift
+++ demoskratos/ViewModels/Authentication.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import Firebase
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        userSession = Auth.auth().currentUser
+    }
+}
