3315847f3e6a905f852cd1b99aab89d100ae497e
chavalk <chavalk@hotmail.com>
2023-06-07 22:56:37 -0500

    Add sign in function to authentication view model.

--- demoskratos/ViewModels/Authentication.swift
+++ demoskratos/ViewModels/Authentication.swift
@@ -15,6 +15,18 @@ class Authentication: ObservableObject {
         userSession = Auth.auth().currentUser
     }
     
+    func signIn(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to sign in with error \(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: Signed user in successfully")
+            print("DEBUG: User id \(result?.user.uid)")
+        }
+    }
+    
     func registerUser(withEmail email: String, password: String, fullName: String) {
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {
