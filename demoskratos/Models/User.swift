4203c2fb48523dd7e23549b9d9ab7c64b705838c
chavalk <chavalk@hotmail.com>
2023-06-08 12:33:27 -0500

    Add user model.

---
+++ demoskratos/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 6/8/23.
+//
+
+import Foundation
+
+struct User: Codable {
+    let fullName: String
+    let email: String
+    let uid: String
+}
