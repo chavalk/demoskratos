4a40aceceb92e7e4e82208ce5841fa713a211f84
chavalk <chavalk@hotmail.com>
2023-06-16 23:52:20 -0500

    Add struct and properties to representative model.

--- demoskratos/Models/Representative.swift
+++ demoskratos/Models/Representative.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Representative: Codable {
+    let uid: String
+    let name: String
+    let district: Int
+    let position: String
+    let profileImageUrl: String
+    let state: String
+    let districtPopulation: Int
+    let districtVap: Int
+}
