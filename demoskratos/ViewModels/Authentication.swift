42d64c74cbdde721bca2c219a2ec6ebba1fd159b
chavalk <chavalk@hotmail.com>
2023-06-17 11:45:43 -0500

    Add fetch representative function.

--- demoskratos/ViewModels/Authentication.swift
+++ demoskratos/ViewModels/Authentication.swift
@@ -66,4 +66,16 @@ class Authentication: ObservableObject {
             print("DEBUG: Email is \(user.email)")
         }
     }
+    
+    func fetchRepresentative() {
+        let uid = "Qzqjtk1sa2IJcbwPQ1PG"
+        Firestore.firestore().collection("representatives").document(uid).getDocument { snapshot, _ in
+            guard let snapshot = snapshot else { return }
+            
+            guard let representative = try? snapshot.data(as: Representative.self) else { return }
+            
+            print("DEBUG: Representative is \(representative.name)")
+            self.representative = representative
+        }
+    }
 }
