09332c895d0b52add1bc778c826cd7db8d7605e2
chavalk <chavalk@hotmail.com>
2023-06-17 17:07:10 -0500

    Replace image with async image in circle image view.

--- demoskratos/Views/Helpers/CircleImage.swift
+++ demoskratos/Views/Helpers/CircleImage.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct CircleImage: View {
     @Environment(\.colorScheme) var colorScheme
-    var image: Image
     
     var body: some View {
-        image
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .frame(width: 250, height: 250)
-            .clipShape(Circle())
-            .overlay {
-                Circle().stroke(colorScheme == .dark ? .black : .white, lineWidth: 4)
-            }
-            .shadow(radius: 7)
+        AsyncImage(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/demoskratos-1160e.appspot.com/o/us-house-tx-21.png?alt=media&token=562aa2d7-39c8-489a-8084-3072dddd0fcd")) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 250, height: 250)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(colorScheme == .dark ? .black : .white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+        } placeholder: {
+            Circle()
+                .frame(width: 250, height: 250)
+                .foregroundColor(Color(uiColor: .lightGray))
+                .overlay {
+                    Circle().stroke(colorScheme == .dark ? .black : .white, lineWidth: 4)
+                    ProgressView()
+                }
+                .shadow(radius: 7)
+        }
     }
 }
 
