771758f873c5c40114a831292fd091d3db7fb244
chavalk <chavalk@hotmail.com>
2023-06-19 10:53:32 -0500

    Add published var, initializer, and fetch votes function to feed view model.

--- demoskratos/ViewModels/FeedViewModel.swift
+++ demoskratos/ViewModels/FeedViewModel.swift
@@ -5,8 +5,20 @@
 //  Created by Jose Garcia on 6/17/23.
 //
 
-import Foundation
+import Firebase
 
 class FeedViewModel: ObservableObject {
+    @Published var votes = [Vote]()
     
+    init() {
+        fetchVotes()
+    }
+    
+    func fetchVotes() {
+        Firestore.firestore().collection("votes").getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            let votes = documents.compactMap({ try? $0.data(as: Vote.self) })
+            self.votes = votes
+        }
+    }
 }
