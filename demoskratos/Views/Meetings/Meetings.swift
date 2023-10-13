c928c2fc8dbd6d63fed6038c6cf6a63b977cf10d
chavalk <chavalk@hotmail.com>
2023-10-12 19:02:02 -0500

    Add chamber seal and meeting status for US house.

--- demoskratos/Views/Meetings/Meetings.swift
+++ demoskratos/Views/Meetings/Meetings.swift
@@ -18,8 +18,32 @@ struct Meetings: View {
                     
                     Spacer()
                 }
+                .padding()
+                
+                HStack {
+                    AsyncImage(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/demoskratos-1160e.appspot.com/o/us-house-seal.png?alt=media&token=94f6c9b6-a84c-4671-aca3-e23fce03ad1e&_gl=1*g5qcny*_ga*OTE0NTQ3NjUyLjE2NzExNDk2NjM.*_ga_CW55HF8NVT*MTY5NzE1MzM2NS4xNDUuMS4xNjk3MTUzNDYyLjM4LjAuMA..")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .frame(width: 56, height: 56)
+                    } placeholder: {
+                        Circle()
+                            .frame(width: 56, height: 56)
+                            .foregroundColor(Color(uiColor: .lightGray))
+                    }
+                    
+                    Text("Next Meeting:")
+                        .bold()
+                    
+                    Text("Currently in session")
+                    
+                    Spacer()
+                }
                 .padding(.leading)
-                .padding(.bottom)
+                
+                Divider()
+                    .padding()
             }
             .navigationTitle("Meetings")
         }
