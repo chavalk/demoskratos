303a7675898705e186f76e23d48f89c8e274eee8
chavalk <chavalk@hotmail.com>
2023-10-14 16:50:40 -0500

    Add representative bio.

---
+++ demoskratos/Views/Profile/RepresentativeBio.swift
@@ -0,0 +1,18 @@
+//
+//  RepresentativeBio.swift
+//  demoskratos
+//
+//  Created by Jose Garcia on 10/14/23.
+//
+
+import SwiftUI
+
+struct RepresentativeBio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepresentativeBio()
+}
