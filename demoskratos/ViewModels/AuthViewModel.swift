c4ce6b3782c0d377c38134ee99d2e1c4dca70edb
chavalk <chavalk@hotmail.com>
2023-10-19 10:46:17 -0500

    Comment out code to clear warning.

--- demoskratos/ViewModels/AuthViewModel.swift
+++ demoskratos/ViewModels/AuthViewModel.swift
@@ -84,7 +84,7 @@ class AuthViewModel: ObservableObject {
         Firestore.firestore().collection("users").document(uid).getDocument { snapshot, _ in
             guard let snapshot = snapshot else { return }
             
-            guard let user = try? snapshot.data(as: User.self) else { return }
+//            guard let user = try? snapshot.data(as: User.self) else { return }
         }
     }
 }
