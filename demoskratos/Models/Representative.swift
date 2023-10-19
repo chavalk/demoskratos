746789df9b623351703dd15d0d538030c0dface7
chavalk <chavalk@hotmail.com>
2023-10-19 00:30:06 -0500

    Update Representative model to match new data added to representative document in Firestore.

--- demoskratos/Models/Representative.swift
+++ demoskratos/Models/Representative.swift
@@ -8,12 +8,36 @@
 import Foundation
 
 struct Representative: Codable {
-    let uid: String
-    let name: String
+    let age: Int
+    let currentResidence: String
+    let dateOfBirth: String
     let district: Int
-    let position: String
+    let districtPopulation: String
+    let educationFirst: String
+    let educationSecond: String
+    let educationThird: String
+    let employerFifth: String
+    let employerFirst: String
+    let employerFourth: String
+    let employerSecond: String
+    let employerSixth: String
+    let employerThird: String
+    let name: String
+    let officeName: String
+    let placeOfBirth: String
+    let professionalExpFifth: String
+    let professionalExpFirst: String
+    let professionalExpFourth: String
+    let professionalExpSecond: String
+    let professionalExpSixth: String
+    let professionalExpThird: String
     let profileImageUrl: String
+    let raisedIn: String
+    let salary: String
     let state: String
-    let districtPopulation: Int
-    let districtVap: Int
+    let uid: String
+    let universityFirst: String
+    let universitySecond: String
+    let universityThird: String
+    let yearsInPower: Int
 }
