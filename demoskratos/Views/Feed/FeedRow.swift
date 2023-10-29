4812f5c1bf1efe0da8558cdae3a61930ea08ed23
chavalk <chavalk@hotmail.com>
2023-10-29 13:42:45 -0500

    Add spacer to ensure feed rows align correctly.

--- demoskratos/Views/Feed/FeedRow.swift
+++ demoskratos/Views/Feed/FeedRow.swift
@@ -42,6 +42,8 @@ struct FeedRow: View {
                     
                     FeedRowButtons()
                 }
+                
+                Spacer()
             }
             
             Divider()
