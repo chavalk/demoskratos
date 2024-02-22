fedae1fea8a78ede2cbd3074ec228cc156332e6e
chavalk <chavalk@hotmail.com>
2024-02-21 20:12:19 -0600

    Add dismiss functionality to x button in watch live view.

--- demoskratos/Views/Video/WatchLiveView.swift
+++ demoskratos/Views/Video/WatchLiveView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct WatchLiveView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         NavigationStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button {
-                            
+                            dismiss()
                         } label: {
                             Image(systemName: "xmark")
                                 .foregroundStyle(Color(uiColor: .white))
