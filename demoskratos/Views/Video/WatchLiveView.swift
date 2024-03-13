fca37cb5e1654e86ae20a77a61e8084df1eb6480
chavalk <chavalk@hotmail.com>
2024-03-13 16:48:12 -0500

    Embed web view in vstack, add spacer below web view, and change background color to black for vstack.

--- demoskratos/Views/Video/WatchLiveView.swift
+++ demoskratos/Views/Video/WatchLiveView.swift
@@ -13,19 +13,24 @@ struct WatchLiveView: View {
     
     var body: some View {
         NavigationStack {
-            WebView(videoID: "9GA6hBK8xK8?si=rcpIIVy69MqZBlWu")
-                .frame(height: 200)
-                .padding(.horizontal)
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Image(systemName: "xmark")
-                                .foregroundStyle(colorScheme == .dark ? Color.white : Color.black)
+            VStack {
+                WebView(videoID: "9GA6hBK8xK8?si=rcpIIVy69MqZBlWu")
+                    .frame(height: 200)
+                    .padding(.horizontal)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                dismiss()
+                            } label: {
+                                Image(systemName: "xmark")
+                                    .foregroundStyle(colorScheme == .dark ? Color.white : Color.black)
+                            }
                         }
-                    }
                 }
+                
+                Spacer()
+            }
+            .background(Color.black)
         }
     }
 }
