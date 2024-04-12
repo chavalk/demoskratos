14d928e0c3ea58a2ebf165d68281867f975079f3
chavalk <chavalk@hotmail.com>
2024-04-12 16:13:41 -0500

    Add db variable to store database for better code readability.

--- demoskratos/ViewModels/FeedViewModel.swift
+++ demoskratos/ViewModels/FeedViewModel.swift
@@ -10,8 +10,10 @@ import Firebase
 class FeedViewModel: ObservableObject {
     @Published var activities = [Activity]()
     
+    private var db = Firestore.firestore()
+    
     func fetchActivity() {
-        Firestore.firestore().collection("activity").order(by: "timestamp", descending: true).addSnapshotListener({ snapshot, _ in
+        db.collection("activity").order(by: "timestamp", descending: true).addSnapshotListener({ snapshot, _ in
             guard let documents = snapshot?.documents else { return }
             let activities = documents.compactMap({ try? $0.data(as: Activity.self) })
             self.activities = activities
