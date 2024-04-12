cd586c9cef48b19c850e0c41b2a76f69b9464498
chavalk <chavalk@hotmail.com>
2024-04-12 15:40:08 -0500

    Remove fetchVotes function from feed view model.

--- demoskratos/ViewModels/FeedViewModel.swift
+++ demoskratos/ViewModels/FeedViewModel.swift
@@ -9,7 +9,6 @@ import Firebase
 import SwiftUI
 
 class FeedViewModel: ObservableObject {
-    @Published var votes = [Vote]()
     @Published var activities = [Activity]()
     var userSession: FirebaseAuth.User?
     var user: User?
@@ -17,7 +16,6 @@ class FeedViewModel: ObservableObject {
     init() {
         userSession = Auth.auth().currentUser
         fetchUser()
-        
     }
     
     func fetchUser() {
@@ -28,34 +26,7 @@ class FeedViewModel: ObservableObject {
             guard let user = try? snapshot.data(as: User.self) else { return }
             self.user = user
             
-            self.fetchVotes()
             self.fetchActivity()
-        }
-    }
-    
-    func fetchVotes() {
-        Firestore.firestore().collection("votes").getDocuments { snapshot, _ in
-            guard let documents = snapshot?.documents else { return }
-            let votes = documents.compactMap({ try? $0.data(as: Votes.self) })
-            for index in votes.indices.reversed() {
-                self.votes.append(contentsOf: votes[index].votes)
-            }
-            for index in 0..<self.votes.count {
-                let vote = self.votes[index]
-                
-                let yesVotes = vote.yesVotes
-                let noVotes = vote.noVotes
-                let notVoting = vote.notVoting
-                let repFullName = self.user?.usHouseRepresentative
-                let repLastName = repFullName?.components(separatedBy: " ").last ?? ""
-                if yesVotes.contains(repLastName) {
-                    self.votes[index].billTitle = "Voted Yes \(vote.question) of \(vote.bill):\(vote.billTitle)"
-                } else if noVotes.contains(repLastName) {
-                    self.votes[index].billTitle = "Voted No \(vote.question) of \(vote.bill):\(vote.billTitle)"
-                } else if notVoting.contains(repLastName) {
-                    self.votes[index].billTitle = "Did not vote on \(vote.question) of \(vote.bill):\(vote.billTitle)"
-                }
-            }
         }
     }
     
