efbe9d058109ad6b46b7882caa12648b769b02cd
chavalk <chavalk@hotmail.com>
2024-04-12 16:18:51 -0500

    Add limit modifier to database call to fetch 30 documents initially.

--- demoskratos/ViewModels/FeedViewModel.swift
+++ demoskratos/ViewModels/FeedViewModel.swift
@@ -13,10 +13,13 @@ class FeedViewModel: ObservableObject {
     private var db = Firestore.firestore()
     
     func fetchActivity() {
-        db.collection("activity").order(by: "timestamp", descending: true).addSnapshotListener({ snapshot, _ in
-            guard let documents = snapshot?.documents else { return }
-            let activities = documents.compactMap({ try? $0.data(as: Activity.self) })
-            self.activities = activities
-        })
+        db.collection("activity")
+            .order(by: "timestamp", descending: true)
+            .limit(to: 30)
+            .addSnapshotListener({ snapshot, _ in
+                guard let documents = snapshot?.documents else { return }
+                let activities = documents.compactMap({ try? $0.data(as: Activity.self) })
+                self.activities = activities
+            })
     }
 }
