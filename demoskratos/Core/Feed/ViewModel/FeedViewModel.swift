cf46a9fda47d3ecf2f203ae5dceb294be87763d8
chavalk <chavalk@hotmail.com>
2024-04-14 16:33:46 -0500

    Remove snapshot listener from database calls in fetch activity function in order to reduce number of reads cause by snapshot listener.

--- demoskratos/Core/Feed/ViewModel/FeedViewModel.swift
+++ demoskratos/Core/Feed/ViewModel/FeedViewModel.swift
@@ -24,24 +24,24 @@ class FeedViewModel: ObservableObject {
                 .order(by: "timestamp", descending: true)
                 .limit(to: 10)
                 .start(afterDocument: lastDocument)
-                .addSnapshotListener({ snapshot, _ in
+                .getDocuments { snapshot, _ in
                     guard let documents = snapshot?.documents else { return }
                     let activities = documents.compactMap({ try? $0.data(as: Activity.self) })
                     self.activities.append(contentsOf: activities)
                     self.lastDocument = documents.last
                     print("Call to database Feed View Model fetchActivity() in if: \(self.activities.count)")
-                })
+                }
         } else {
             db.collection("activity")
                 .order(by: "timestamp", descending: true)
                 .limit(to: 10)
-                .addSnapshotListener({ snapshot, _ in
+                .getDocuments { snapshot, _ in
                     guard let documents = snapshot?.documents else { return }
                     let activities = documents.compactMap({ try? $0.data(as: Activity.self) })
                     self.activities = activities
                     self.lastDocument = documents.last
                     print("Call to database Feed View Model fetchActivity() in else: \(self.activities.count)")
-                })
+                }
         }
     }
 }
