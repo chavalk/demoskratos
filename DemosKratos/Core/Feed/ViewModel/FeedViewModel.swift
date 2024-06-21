c7615d0b889d2fa2b18f0875f1a83f5a42f581e7
chavalk <chavalk@hotmail.com>
2024-06-20 21:11:41 -0500

    Add fetch current user function in feed view model.

--- DemosKratos/Core/Feed/ViewModel/FeedViewModel.swift
+++ DemosKratos/Core/Feed/ViewModel/FeedViewModel.swift
@@ -10,10 +10,12 @@ import Firebase
 class FeedViewModel: ObservableObject {
     @Published var isFetching: Bool = true
     @Published var representative: Representative?
+    var currentUser: User?
     
     @MainActor
     func fetchRepresentative() async {
         do {
+            fetchCurrentUser()
             let doc = try await Firestore.firestore().collection("representatives").document("Roy").getDocument()
             let fetchedRepresentative = try? doc.data(as: Representative.self)
             representative = fetchedRepresentative
@@ -22,5 +24,14 @@ class FeedViewModel: ObservableObject {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    @MainActor
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+        print("Call to database Feed View Model fetchCurrentUser()")
     }
 }
