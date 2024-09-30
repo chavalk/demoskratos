c3718908e73c68862bee8b924cccb5414f1ee2ad
chavalk <chavalk@hotmail.com>
2024-09-29 21:57:37 -0500

    Update app name on landing screen.

--- DemosKratos/Core/Authentication/View/Landing.swift
+++ DemosKratos/Core/Authentication/View/Landing.swift
@@ -17,7 +17,7 @@ struct Landing: View {
                 Spacer()
                 
                 VStack(alignment: .center, spacing: 25) {
-                    Text("Demos Kratos")
+                    Text("Demoskratos")
                         .font(.system(size: 55))
                         .fontWeight(.bold)
                     
